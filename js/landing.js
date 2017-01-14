aef0dcc5ec22757435186f5460eac2100aae6e22
Steve Dillon <stevedillon001@gmail.com>
2017-01-14 12:28:35 -0800

    Completed Checkpoint 8 Assignment Work

--- js/landing.js
+++ js/landing.js
@@ -1,4 +1,39 @@
+//Refactored code to reveal ALL points by calling function animatePoints();
 
+var animatePoints = function() {
+	var points = document.getElementsByClassName('point');
+
+		var revealPoint = function(index) {
+				points[index].style.opacity = 1;
+				points[index].style.transform = "scaleX(1) translateY(0)";
+				points[index].style.msTransform = "scaleX(1) translateY(0)";
+				points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+		}
+
+	for (var i = 0; i < points.length; i++) {
+		revealPoint(i);
+	}
+};
+
+// Reveal points one by one based on argument passed in, i.e. revealPoint(2);
+
+/*var revealPoint = function(index) {
+
+	var points = document.getElementsByClassName('point');
+
+	for (var i = 0; i < points.length; i++) {
+		points[index].style.opacity = 1;
+		points[index].style.transform = "scaleX(1) translateY(0)";
+		points[index].style.msTransform = "scaleX(1) translateY(0)";
+		points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+
+	}
+}
+*/
+
+//Long code to reveal ALL points by calling function animatePoints();
+
+/*
 var animatePoints = function() {
 
 	var points = document.getElementsByClassName('point');
@@ -29,3 +64,4 @@ var animatePoints = function() {
 	revealThirdPoint();
 
 };
+*/
